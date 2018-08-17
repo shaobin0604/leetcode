03ea990fc8ec0e47b23188b00707a6388dcfaa8b
Bin Shao <bin.shao@56qq.com>
2018-08-17 22:35:46 +0800

    feat: add solution to Array 24
    https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/24/

---
+++ src/main/java/Array24.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Array24 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> marks = new HashSet<>();
+
+        for (int num : nums) {
+            if (marks.contains(num)) {
+                return true;
+            } else {
+                marks.add(num);
+            }
+        }
+
+        return false;
+    }
+}
