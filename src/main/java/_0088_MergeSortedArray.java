7a638264bc1f1134d8c838a1d765317fe2fa92d5
Bin Shao <bin.shao@56qq.com>
2019-08-14 14:18:30 +0800

    feat: add 88.Merge Sorted Array solution

---
+++ src/main/java/_0088_MergeSortedArray.java
@@ -0,0 +1,36 @@
+public class _0088_MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        // 循环结束条件，nums2 数组中的元素已经全部放入 nums1
+        while (j >= 0) {
+            if (i < 0 || nums1[i] <= nums2[j]) {
+                nums1[k] = nums2[j];
+                j--;
+            } else {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            k--;
+        }
+    }
+
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        // two get pointers for nums1 and nums2
+        int p1 = m - 1;
+        int p2 = n - 1;
+        // set pointer for nums1
+        int p = m + n - 1;
+
+        // while there are still elements to compare
+        while ((p1 >= 0) && (p2 >= 0))
+            // compare two elements from nums1 and nums2
+            // and add the largest one in nums1
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+        // add missing elements from nums2
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+    }
+}
---
+++ src/test/java/_0088_MergeSortedArrayTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class _0088_MergeSortedArrayTest {
+    @Test
+    public void test_case1() {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        int m = 3;
+        int n = 3;
+
+        new _0088_MergeSortedArray().merge(nums1, m, nums2, n);
+        final int[] expected = {1, 2, 2, 3, 5, 6};
+        Assert.assertArrayEquals(expected, nums1);
+    }
+
+    @Test
+    public void test_case2() {
+        int[] nums1 = {0};
+        int[] nums2 = {1};
+        int m = 0;
+        int n = 1;
+
+        new _0088_MergeSortedArray().merge(nums1, m, nums2, n);
+        final int[] expected = {1};
+        Assert.assertArrayEquals(expected, nums1);
+    }
+}
